07925f545b2f84d4320acbcc35afb7b5eca1f964
MineevRA <alone.rusya.mineev@mail.ru>
2022-07-13 23:04:05 +0300

    initial-commit1

--- module_01/task01/src/Main.java
+++ module_01/task01/src/Main.java
@@ -1,5 +1,8 @@
+import java.time.LocalDateTime;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Текущая дата: " + LocalDateTime.now().toLocalDate());
+        System.out.println("Текущая время: " + LocalDateTime.now().toLocalTime());
     }
 }
